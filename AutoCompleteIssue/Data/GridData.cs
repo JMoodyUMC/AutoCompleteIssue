70d9bf98e52cb72c4e6ef0e7c79614228991ca9b
Jill Ellen Moody <JillEMoody@sbcglobal.net>
2021-10-27 11:00:07 -0400

    Add project files.

---
+++ AutoCompleteIssue/Data/GridData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoCompleteIssue.Data
+{
+    public class GridData
+    {
+        public String Code { get; set; }
+    }
+}
